5ae92b43a8197c3d6ea40a03e020df977fbcfaaa
Moritz Wilhelmy <mw@barfooze.de>
2022-10-27 17:16:53 +0200

    attempt to gather topology data from all nodes

--- lldpcli-json.go
+++ lldpcli-json.go
@@ -10,14 +10,20 @@ import (
 )
 
 const dbg_json_verbose = true
-const dbg_read_json_from_file = true
+const dbg_read_json_from_file = false
 
 func run_lldpcli_show(arg string) ([]byte, error) {
 	var res []byte
 	var err error
 
 	if dbg_read_json_from_file { // i.e. for development purposes on x86 host
-		res, err = os.ReadFile(fmt.Sprintf("./examples/lldpcli-show-%s.json", arg))
+		fd, err := os.Open(fmt.Sprintf("./examples/lldpcli-show-%s.json", arg))
+		if err != nil {return nil,err}
+		res := make([]byte, 9999)
+		_, err = fd.Read(res)
+		if err != nil {return nil,err}
+		fd.Close()
+
 
 	} else {
 		cmd := exec.Command("lldpcli", "-f", "json", "show", arg)
@@ -32,7 +38,9 @@ func run_lldpcli_show(arg string) ([]byte, error) {
 }
 
 // JSON data structures are currently automatically converted to Go using
-// https://mholt.github.io/json-to-go/
+// https://mholt.github.io/json-to-go/ and then manually pieced apart
+// into substructures.  The names dc3500, en0 and en1 are project specific and
+// hardcoded due to limitations in the JSON API.
 
 type ChassisInfo struct {
 	LocalChassis struct {
@@ -53,91 +61,104 @@ type ChassisInfo struct {
 	} `json:"local-chassis,omitempty"`
 }
 
+type NeighborChassis struct {
+	Dc3500 struct {
+		ID struct {
+			Type  string `json:"type,omitempty"`
+			Value string `json:"value,omitempty"`
+		} `json:"id,omitempty"`
+		Descr      string `json:"descr,omitempty"`
+		MgmtIP     string `json:"mgmt-ip,omitempty"`
+		Capability []struct {
+			Type    string `json:"type,omitempty"`
+			Enabled bool   `json:"enabled,omitempty"`
+		} `json:"capability,omitempty"`
+	} `json:"dc3500,omitempty"`
+}
+type NeighborPortID struct {
+	Type  string `json:"type,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+type NeighborPort struct {
+	ID    NeighborPortID `json:"id,omitempty"`
+	Descr string         `json:"descr,omitempty"`
+	TTL   string         `json:"ttl,omitempty"`
+}
+
+type NeighborInterface struct {
+	Via     string          `json:"via,omitempty"`
+	Rid     string          `json:"rid,omitempty"`
+	Age     string          `json:"age,omitempty"`
+	Chassis NeighborChassis `json:"chassis,omitempty"`
+	Port    NeighborPort    `json:"port,omitempty"`
+}
+
 type NeighborInfo struct {
 	Lldp struct {
 		Interface []struct {
-			En0 struct {
-				Via     string `json:"via,omitempty"`
-				Rid     string `json:"rid,omitempty"`
-				Age     string `json:"age,omitempty"`
-				Chassis struct {
-					Dc3500 struct {
-						ID struct {
-							Type  string `json:"type,omitempty"`
-							Value string `json:"value,omitempty"`
-						} `json:"id,omitempty"`
-						Descr      string `json:"descr,omitempty"`
-						MgmtIP     string `json:"mgmt-ip,omitempty"`
-						Capability []struct {
-							Type    string `json:"type,omitempty"`
-							Enabled bool   `json:"enabled,omitempty"`
-						} `json:"capability,omitempty"`
-					} `json:"dc3500,omitempty"`
-				} `json:"chassis,omitempty"`
-				Port struct {
-					ID struct {
-						Type  string `json:"type,omitempty"`
-						Value string `json:"value,omitempty"`
-					} `json:"id,omitempty"`
-					Descr string `json:"descr,omitempty"`
-					TTL   string `json:"ttl,omitempty"`
-				} `json:"port,omitempty"`
-			} `json:"en0,omitempty"`
-			En1 struct {
-				Via     string `json:"via,omitempty"`
-				Rid     string `json:"rid,omitempty"`
-				Age     string `json:"age,omitempty"`
-				Chassis struct {
-					Dc3500 struct {
-						ID struct {
-							Type  string `json:"type,omitempty"`
-							Value string `json:"value,omitempty"`
-						} `json:"id,omitempty"`
-						Descr      string `json:"descr,omitempty"`
-						MgmtIP     string `json:"mgmt-ip,omitempty"`
-						Capability []struct {
-							Type    string `json:"type,omitempty"`
-							Enabled bool   `json:"enabled,omitempty"`
-						} `json:"capability,omitempty"`
-					} `json:"dc3500,omitempty"`
-				} `json:"chassis,omitempty"`
-				Port struct {
-					ID struct {
-						Type  string `json:"type,omitempty"`
-						Value string `json:"value,omitempty"`
-					} `json:"id,omitempty"`
-					Descr string `json:"descr,omitempty"`
-					TTL   string `json:"ttl,omitempty"`
-				} `json:"port,omitempty"`
-			} `json:"en1,omitempty"`
+			// TODO: This part of lldpcli -f json output is slightly weird and
+			// doesn't work well with go's json library, hence the hardcoded
+			// interface names. Seems like -f json0 would avoid this. (But would
+			// it avoid the hardcoded Dc3500 in NeighborChassis? Evaluate...)
+			En0 *NeighborInterface  `json:"en0,omitempty"`
+			En1 *NeighborInterface  `json:"en1,omitempty"`
 		} `json:"interface,omitempty"`
 	} `json:"lldp,omitempty"`
 }
 
-func json_dbg(obj interface{}) {
+// conditionally log message to see if data parses correctly into the struct
+func dbg_json(obj interface{}) {
 	if dbg_json_verbose {
-		log.Print("Re-Marshaled JSON: ")
-		log.Println(json.Marshal(&obj))
+		json, err := json.Marshal(&obj)
+		log.Print("Re-Marshaled JSON, error=", err, " -- ", string(json))
 	}
 }
 
-func lldp_chassis_data(b []byte) (*ChassisInfo, error) {
+// parses "lldpcli -f json show chassis" output
+func lldp_parse_chassis_data(b []byte) (*ChassisInfo, error) {
 	var c ChassisInfo
 	if err := json.Unmarshal(b, &c); err != nil {
 		return nil, err
 	}
 
-	json_dbg(c);
-	return &c, nil;
+	dbg_json(c)
+	return &c, nil
 }
 
-func lldp_neighbor_data(b []byte) (*NeighborInfo, error) {
+// parses "lldpcli -f json show chassis" output, returns en0+en1 netif member
+// struct as a slice
+func lldp_parse_neighbor_data(b []byte) ([]NeighborInterface, error) {
 	var n NeighborInfo
 
 	if err := json.Unmarshal(b, &n); err != nil {
 		return nil, err
 	}
 
-	json_dbg(n);
-	return &n, nil;
+	dbg_json(n)
+
+	var en0 *NeighborInterface = nil
+	var en1 *NeighborInterface = nil
+
+	if len(n.Lldp.Interface) > 0 {
+		if n.Lldp.Interface[0].En0 != nil && n.Lldp.Interface[0].En1 != nil {
+			return nil, fmt.Errorf("lldp_parse_neighbor_data: iface 0 is both en0 and en1")
+		}
+		en0 = n.Lldp.Interface[0].En0
+		en1 = n.Lldp.Interface[0].En1
+	}
+	if len(n.Lldp.Interface) > 1 {
+		if n.Lldp.Interface[1].En0 != nil && n.Lldp.Interface[1].En1 != nil {
+			return nil, fmt.Errorf("lldp_parse_neighbor_data: iface 1 is both en0 and en1")
+		}
+
+		if en0 == nil { en0 = n.Lldp.Interface[1].En0 }
+		if en1 == nil { en1 = n.Lldp.Interface[1].En1 }
+	}
+
+	ret := make([]NeighborInterface, 2)
+	if en0 != nil { ret[0] = *en0 }
+	if en1 != nil { ret[1] = *en1 }
+
+	return ret, nil
 }
--- topologyd.go
+++ topologyd.go
@@ -1,14 +1,21 @@
 package main
 
 import (
-    //"fmt"
+    "fmt"
     "time"
-    //"errors"
+    "io"
     "log"
-    //"strings"
     "net/http"  // Currently communicates only over a simple HTTP interface
 )
 
+const dbg_http_query_verbose = false
+
+const port = 9090
+const lldp_neighbor_path = "/lldp/neighbors"
+const lldp_chassis_path  = "/lldp/chassis"
+
+// Handler function for incoming HTTP requests to query the local node's
+// chassis or neighbor info
 func handle_lldp_request(w http.ResponseWriter, req *http.Request) {
     reqName := req.URL.Path[6:] // cut leading /lldp/
     switch reqName {
@@ -34,16 +41,116 @@ func handle_lldp_request(w http.ResponseWriter, req *http.Request) {
     }
 }
 
+// Send HTTP GET requests to specified node, logs errors and discards malformed
+// responses to keep the rest of the logic clean
+func http_get(host string, path string) []byte {
+    // send request
+    url := fmt.Sprintf("http://%s:%d%s", host, port, path)
+    resp, err := http.Get(url)
+    if err != nil {
+        log.Printf("Error querying %s: %s\n", url, err)
+        return nil
+    } else if dbg_http_query_verbose {
+        log.Printf("HTTP response for GET %s: %s\n", url, resp)
+    }
+    defer resp.Body.Close()
 
+    if resp.StatusCode != http.StatusOK {
+        log.Printf("HTTP GET %s returned code %d %s: %s\n",
+            url, resp.StatusCode, resp.Status, resp)
+        return nil
+    }
+
+    contentType := resp.Header.Get("Content-type")
+    if contentType != "application/json" {
+        log.Printf("HTTP GET %s did not return JSON: %s\n", url, resp)
+        return nil
+    }
+
+    // read response body
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        log.Printf("Error reading response body for GET %s: %s\n", url, err)
+        return nil
+    } else if dbg_http_query_verbose {
+        log.Printf("Body: %s\n", body)
+    }
+    return body
+}
+
+// the more wrappers, the better, right?
+func get_node_neighbor_info(host string) []NeighborInterface {
+    data := http_get(host, lldp_neighbor_path)
+    if data == nil { return nil }
+
+    // parse result as JSON
+    ifaces, err := lldp_parse_neighbor_data(data)
+    if err != nil {
+        log.Print(err)
+    }
+
+    return ifaces
+}
+
+func get_localhost_mgmt_ip() string {
+    const host = "localhost"
+    data := http_get(host, lldp_chassis_path)
+    if data == nil { return "" }
+
+    chassisptr, err := lldp_parse_chassis_data(data)
+    if err != nil {
+        log.Print(err)
+    }
+
+    return (*chassisptr).LocalChassis.Chassis.Dc3500.MgmtIP[0]
+}
+
+// Crawl the entire network for LLDP neighbors
+func gather_neighbors_from_nodes() {
+    // preallocate this many hashtable entries
+    const prealloc = 32
+    // hashmap keyed by MgmtIP addresses
+    neighbors := make(map[string]*[]NeighborInterface, prealloc)
+
+    ip   := get_localhost_mgmt_ip()
+    todo := []string{ ip }
+
+    for {
+        log.Print(todo)
+        var cur []NeighborInterface = get_node_neighbor_info(ip)
+        if cur == nil {goto next} // error is logged in function call
+        neighbors[ip] = &cur
+
+        for i, _ := range cur {
+            newip := cur[i].Chassis.Dc3500.MgmtIP
+
+            _, found := neighbors[newip]
+            if !found {
+                // initialize hashtable spot to prevent adding to todo twice
+                neighbors[newip] = nil
+                todo = append(todo, newip)
+            }
+        }
+
+    next:
+        if len(todo) <= 1 {
+            break
+        }
+        ip, todo = todo[0], todo[1:]
+    }
+
+    log.Print("here")
+}
 
 func main() {
-    http.HandleFunc("/lldp/neighbors", handle_lldp_request)
-    http.HandleFunc("/lldp/chassis",   handle_lldp_request)
-    go http.ListenAndServe(":9090", nil) // start separate goroutine for httpd
+    http.HandleFunc(lldp_neighbor_path, handle_lldp_request)
+    http.HandleFunc(lldp_chassis_path,  handle_lldp_request)
 
+    // start separate goroutine for httpd
+    go http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 
-
-
+    time.Sleep(1)
+    gather_neighbors_from_nodes()
     for {
         time.Sleep(1)
     }
